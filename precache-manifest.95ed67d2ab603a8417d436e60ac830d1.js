dfd643e6acc98684ac66718a9252797fc7befab6
Yash <pathakyash427@gmail.com>
2022-01-18 12:18:06 +0530

    Updates

---
+++ precache-manifest.95ed67d2ab603a8417d436e60ac830d1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f58f62500142bb68f2e49d169151e7d",
+    "url": "/simple-counter/index.html"
+  },
+  {
+    "revision": "c0e544843cfa412027e8",
+    "url": "/simple-counter/static/js/2.97bd378f.chunk.js"
+  },
+  {
+    "revision": "e16c12aee91f1ddcf16d",
+    "url": "/simple-counter/static/js/main.66f5549a.chunk.js"
+  },
+  {
+    "revision": "79252a3b289b7bcc2858",
+    "url": "/simple-counter/static/js/runtime-main.62140dbe.js"
+  }
+]);
